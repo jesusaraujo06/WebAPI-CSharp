81619fccac594b49d31fa0728b9b82ed7bc1aa74
Jesus Araujo <jesus.araujo@coolechera.com>
2023-01-11 16:26:37 -0500

    Agregar archivos de proyecto.

---
+++ WebAPI-CSharp/Controllers/BooksController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI_CSharp.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BooksController
+    {
+
+    }
+}
---
+++ WebAPI-CSharp/Models/Book.cs
@@ -0,0 +1,10 @@
+﻿namespace WebAPI_CSharp.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public bool IsAvailable { get; set; }
+    }
+}
